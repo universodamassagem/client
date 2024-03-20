73fdf5a4b02989ba24f0c50b4d40042bce07a3f1
universodamassagem <102084970+universodamassagem@users.noreply.github.com>
2024-03-20 14:03:57 -0300

    telephone dend-whatsapp

--- src/functions/send-whatsapp.js
+++ src/functions/send-whatsapp.js
@@ -1,5 +1,5 @@
 const WMsg = 'Oi,%20gostaria%20de%20marcar%20uma%20consulta!';
 
-const FONE_NUMBER = '5541998124378'
+const FONE_NUMBER = '5541991346013'
 
 export default function send(message) { window.open(`https://wa.me/${FONE_NUMBER}/?text=${message ? message : WMsg}`) }
